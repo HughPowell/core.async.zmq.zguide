71ff47fd56527b2361fc1f007b2ee0dd6a4faf9e
Hugh Powell <hugh@hughpowell.co.uk>
2015-06-13 11:22:42 +0100

    Simplify the hwserver

--- src/core/async/zmq/hwserver.clj
+++ src/core/async/zmq/hwserver.clj
@@ -2,24 +2,13 @@
   (:require [clojure.core.async :as async]
             [core.async.zmq :as zmq]))
 
-(defn hello-world-server
+(defn -main []
   "Hello World server
   Binds REP socket to tcp://*:5555
   Expects 'Hello' from client, replies with 'World'"
-  [result]
-  (async/go-loop
-   [server (zmq/chan :rep true :tcp "*:5555")]
-   ;; Wait for next request from client
-   (async/>! result (str "Received" (async/<! server)))
-
-   ;; Do some "work"
-   (async/<! (async/timeout 1))
-
-   ;; Send reply back to client
-   (async/>! server "World")
-   (recur server)))
-
-(defn -main []
-  (let [chan (async/chan)]
+  (let [server (zmq/chan :rep :bind :tcp "*:5555")]
     (while true
-      (println (async/<!! (hello-world-server chan))))))
+      (println "Received" (async/<!! server))
+      ;; Do some "work"
+      (async/<!! (async/timeout 1000))
+      (async/>!! server "World"))))
