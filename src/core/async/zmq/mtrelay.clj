095061ccc4b77e7331bda04fbab566dbcff8af40
Hugh Powell <hugh@hughpowell.co.uk>
2015-06-15 21:09:10 +0100

    Make mtrelay work deterministically

--- src/core/async/zmq/mtrelay.clj
+++ src/core/async/zmq/mtrelay.clj
@@ -2,26 +2,27 @@
   (:require [clojure.core.async :as async]
             [core.async.zmq :as zmq]))
 
-(defn -main
-  "Multithreaded relay"
-  []
+(defn step1 []
   (async/go
    (println "Step 1 ready, signalling step 2")
    (->
     (zmq/chan :pair :connect :inproc "step2")
-    (async/>! "READY")))
+    (async/>! "READY"))))
 
+(defn step2 []
   (async/go
-   (->
-    (zmq/chan :pair :bind :inproc "step2")
-    async/<!)
+   (let [receiver (zmq/chan :pair :bind :inproc "step2")]
+     (step1)
+     (async/<! receiver))
    (println "Step 2 ready, signalling step 3")
    (->
     (zmq/chan :pair :connect :inproc "step3")
-    (async/>! "READY")))
+    (async/>! "READY"))))
 
-  (->
-   (zmq/chan :pair :bind :inproc "step3")
-   async/<!!)
-
-  (println "Test succeful!"))
+(defn -main
+  "Multithreaded relay"
+  []
+  (let [receiver (zmq/chan :pair :bind :inproc "step3")]
+    (step2)
+    (async/<!! receiver)
+    (println "Test succeful!")))
