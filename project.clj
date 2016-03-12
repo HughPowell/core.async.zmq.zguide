35e0d60b1a4a4e392bc298f1cb47c7ae20555c01
Hugh Aristotle Powell <hugh@hughpowell.co.uk>
2016-03-12 17:28:40 +0000

    Ignore Intellij Idea files. Update dependencies.

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/HughPowell/core.async.zmq.zguide"
   :license {:name "Mozilla Public License Version 2.0"
             :url "https://www.mozilla.org/MPL/2.0/"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.0-SNAPSHOT"]
-                 [org.zeromq/jeromq "0.3.5-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.2.374"]
+                 [org.zeromq/jeromq "0.3.5"]
 ;;                 [org.zeromq/jzmq "3.0.1"]
                  [core.async.zmq "0.1.0-SNAPSHOT"]]
   :repositories [["releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
